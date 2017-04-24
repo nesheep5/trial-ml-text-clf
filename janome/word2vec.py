2ac86b02285a115920a00f14f44fafb41fc40d68
Shogo Watanabe <ne.sheep.5mouths@gmail.com>
2017-04-24 13:05:53 +0900

    テキストの不要な情報削除処理を修正

--- janome/word2vec.py
+++ janome/word2vec.py
@@ -7,20 +7,20 @@ bindata = open('kokoro.txt.sjis', 'rb').read()
 text = bindata.decode('shift_jis')
 
 # テキストの先頭にあるヘッダーとフッターを削除
-text = re.split(r'\-{5}', text)[2]
-text = re.split(r'底本:', text)[0]
+text = re.split(r'\-{5,}',text)[2]
+text = re.split(r'底本：', text)[0]
 text = text.strip()
 
 # 形態素解析
 t = Tokenizer()
 results = []
 # テキストを一行ずつ処理する
-lines = text.split("¥r¥n")
+lines = text.split("\r\n")
 for line in lines:
     s = line
-    s = s.replace('|', '')
-    s = re.sub(r'《.+?》','',  s) # ルビを削除
-    s = re.sub(r'[#.+?]','', s) # 入力注を削除
+    s = s.replace('｜', '')
+    s = re.sub(r'《.+?》', '', s) # ルビを削除
+    s = re.sub(r'［＃.+?］', '', s) # 入力注を削除
     tokens = t.tokenize(s) # 形態素解析
     # 必要な語句だけを対象とする
     r = []
@@ -40,10 +40,11 @@ for line in lines:
 # 書き込み先テキストを開く
 wakati_file = 'kokoro.wakati'
 with open(wakati_file, 'w', encoding='utf-8') as fp:
-    fp.write("¥n".join(results))
+    fp.write("\n".join(results))
 
 # Word2Vecでモデルを作成
 data = word2vec.LineSentence(wakati_file)
-model = word2vec.Word2Vec(data, size=200, window=10, hs=1, min_count=1, sg=1)
+model = word2vec.Word2Vec(data,
+    size=200, window=10, hs=1, min_count=2, sg=1)
 model.save('kokoro.model')
 print('ok')

abb77944c3d2801e5721eaf9abee828e7b9dec41
shogo watanabe <ne.sheep.5mouths@gmail.com>
2017-05-16 21:45:11 +0900

    マルコフ連鎖を実装

--- janome/markov/markov.py
+++ janome/markov/markov.py
@@ -16,3 +16,31 @@ def make_dic(words):
             tmp = ["@"]
             continue
     return dic
+
+# 三要素のリストを辞書として登録
+def set_word3(dic, s3):
+    w1, w2, w3 = s3
+    if not w1 in dic: dic[w1] = {}
+    if not w2 in dic[w1]: dic[w1][w2] = {}
+    if not w3 in dic[w1][w2]: dic[w1][w2][w3] = 0
+    dic[w1][w2][w3] += 1
+
+# 作文する
+def make_sentence(dic):
+    ret = []
+    if not "@" in dic: return "no dic"
+    top = dic["@"]
+    w1 = word_choice(top)
+    w2 = word_choice(top[w1])
+    ret.append(w1)
+    ret.append(w2)
+    while True:
+        w3 = word_choice(dic[w1][w2])
+        ret.append(w3)
+        if w3 == "。": break
+        w1, w2 = w2, w3
+    return "".join(ret)
+
+def word_choice(sel):
+    keys = sel.keys()
+    return random.choise(list(leys))

e6169a36da564310279f415d2881ad74a50814ad
shogo watanabe <ne.sheep.5mouths@gmail.com>
2017-05-22 22:06:20 +0900

    LSTMの実装

--- janome/markov/lstm-text-gen.py
+++ janome/markov/lstm-text-gen.py
@@ -52,3 +52,37 @@ def sample(prads, tmperature=1.0):
     exp_preds = np.exp(preds)
     preds = exp_preds / np.sum(exp_preds)
     probas = np.random.multinomal(1, prads, 1)
+    return np.argmax(probas)
+
+# 学習させて、テキストを生成する・・・を繰り返す
+for iteration in range(1, 60):
+    print()
+    print('-' * 50)
+    print('繰り返し=', iteration)
+    model.fit(X, y, batch_size=128, nb_epoch=1) #訓練
+    # ランダムにテキストのシードを選ぶ
+    start_index = random.randint(0, len(text) - maxlrn -1)
+    # 多様性のパラメータごとに文を生成する
+    for diversity in [0.2, 0.5, 1.0, 1.2]:
+        print()
+        print("---多様性=", diversity):
+        generated = ""
+        sentence = text[start_index: start_index + maxlen]
+        generated += sentence
+        print("---シード=" + sentence + '"')
+        sys.stdout.write(generated)
+        # シードを元にテキストを自動で生成する
+        for i in range(400):
+            x = np.zeros((1, maxlen, len(chars)))
+            for t, char in enumerate(sentence):
+                x[0, t, char_indices[char]] = 1.
+            # 次に来る文字を予測
+            preds = model.predict(x, verbose=0)[0]
+            next_index = sample(preds, diversity)
+            next_char = indices_char[next_index]
+            # 既存の文に予測した一文字を足す
+            generated +=  next_char
+            sentence = sentence[1:] + next_char
+            sys.stdout.write(next_char)
+            sys.stdout.flush()
+        print()

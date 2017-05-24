41265b96190efe25345207dfdb1313b008e9cd79
shogo watanabe <ne.sheep.5mouths@gmail.com>
2017-05-24 23:40:31 +0900

    記述間違いを修正

--- lstm/lstm-text-gen.py
+++ lstm/lstm-text-gen.py
@@ -1,44 +1,44 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.layers import LSTM
-from keras.optimazers import RMSprop
+from keras.optimizers import RMSprop
 from keras.utils.data_utils import get_file
 import numpy as np
 import random, sys
 
 # 元となるテキストを読み込む
-path = "./text/夏目漱石/吾輩は猫である.txt"
+path = "../data/text/夏目漱石/吾輩は猫である.txt"
 bindata = open(path, "rb").read()
 text = bindata.decode("shift_jis")
-print('コーパスの長さ：', len(text))
+print('コーパスの長さ:', len(text))
 
 # 一文字ずつバラバラにして文字にIDを振る
 chars = sorted(list(set(text)))
 print('使われている文字の数:', len(chars))
-char_indices = dict((c, i) for i, c in enumerate(chars)) #文字→ID
-indices_char = dict((i, c) for i, c,in enumerate(chars)) #ID→文字
+char_indices = dict((c, i) for i, c in enumerate(chars)) # 文字→ID
+indices_char = dict((i, c) for i, c in enumerate(chars)) # ID→文字
 
 # テキストをmaxlen文字で区切って、その文の次にくる文字を記録する
 maxlen = 20
 step = 3
 sentences = []
 next_chars = []
-for i in range(0, len(text)) - maxlen, step):
+for i in range(0, len(text) - maxlen, step):
     sentences.append(text[i: i + maxlen])
     next_chars.append(text[i + maxlen])
 print("学習する文の数:", len(sentences))
 
-print("テキストをIDベクトルにします...")
+print('テキストをIDベクトルにします...')
 X = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
 y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
-for i, sentence in  enumerate(sentences):
+for i, sentence in enumerate(sentences):
     for t, char in enumerate(sentence):
         X[i, t, char_indices[char]] = 1
     y[i, char_indices[next_chars[i]]] = 1
 
 # モデルを構築する(LSTM)
 print("モデルを構築します...")
-model = Sequentical()
+model = Sequential()
 model.add(LSTM(128, input_shape=(maxlen, len(chars))))
 model.add(Dense(len(chars)))
 model.add(Activation('softmax'))
@@ -65,7 +65,7 @@ for iteration in range(1, 60):
     # 多様性のパラメータごとに文を生成する
     for diversity in [0.2, 0.5, 1.0, 1.2]:
         print()
-        print("---多様性=", diversity):
+        print("---多様性=", diversity)
         generated = ""
         sentence = text[start_index: start_index + maxlen]
         generated += sentence
